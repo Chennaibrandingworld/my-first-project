c904a72ce5c58499cf6978be1e67a26d81cda4f5
barathibranding <barathi.brandingworld@gmail.com>
2025-08-31 23:17:58 +0530

    Added Home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Home from "./Component/Home";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Home />
     </div>
   );
 }
